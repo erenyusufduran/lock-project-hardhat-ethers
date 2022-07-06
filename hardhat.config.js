643ed00fb05c12522ad426599acc639bcf9339ff
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-07 01:46:27 +0300

    alchemy_api_key for forking with hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,14 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
     solidity: "0.8.2",
     networks: {
+
+      hardhat: {
+        forking: {
+          url: process.env.ALCHEMY_API_KEY,
+          // blockNumber: 1234567
+        }
+      },
+
       mainnet: {
         url: `https://api.avax.network/ext/bc/C/rpc`,
           accounts: [`${PRIVATE_KEY}`]
